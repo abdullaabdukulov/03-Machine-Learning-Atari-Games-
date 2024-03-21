dcdd547b6802560844d4028160615362a1313fc5
Ahrorjon <151090046+ahrorhaidarov@users.noreply.github.com>
2024-03-21 23:22:20 +0500

    Add files via upload

--- Pacman/train.py
+++ Pacman/train.py
@@ -1,8 +1,15 @@
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 
-vec_env = make_vec_env("MsPacman-v4")
+env_id = "MsPacman-v0"
+num_envs = 4
+vec_env = make_vec_env(env_id, n_envs=num_envs)
+
 
 model = PPO('MlpPolicy', vec_env)
 model.learn(total_timesteps=2500)
+
+
 model.save("ppo_pacman")
+
+
